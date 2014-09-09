fd3a4054b890a769d945dc60d20b2bcef88221bc
minglu <minglu@minglu-virtual-machine.(none)>
2014-09-09 21:38:24 +0800

    change code position

--- sed.sh
+++ sed.sh
@@ -1,39 +1,92 @@
 #!/bin/bash
-sed -n '/special 1,2/{
-    s/line/test/pi
-    }' input
+#sed命令使用实例
+
+echo 替换 -n:只显示处理行 -e:多个匹配 /w filename写入文件 p打印处理结果 g全局替换
+sed -n -e 's/test/trial/gp; s/script/basic/w sed.tmp' sed.data
 echo '==================================='
-sed '/special 1,4/d' input
+echo 限定操作的行
+sed -n '2,${
+    s/test/trial/2p
+    =
+    }' sed.data
 echo '==================================='
-sed '1,$i\new line' input
+sed -n '/^that/s/test/trial/2p' sed.data
 echo '==================================='
-sed '1,$a\new line' input
+
+echo 删除行
+sed '1,4d' sed.data
 echo '==================================='
-sed '3c\chang this line' input
+
+echo 插入行
+sed '1,$i\new line' sed.data
 echo '==================================='
-sed '1,$y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' input
+
+echo 追加行
+sed '1,$a\new line' sed.data
 echo '==================================='
-sed '1,2w output' input
+
+echo 修改行
+sed '3c\chang this line' sed.data
 echo '==================================='
-sed '3r output' input
+sed '2,3c\chang this line' sed.data
 echo '==================================='
-sed -n 's/line//gp' input 
+
+echo 转换，使用前半部分inchar替换为后半部分outchar
+sed '1,$y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' sed.data
 echo '==================================='
-sed '/special/{n; d}' input
+
+echo 写入文件
+sed '1,2w sed.tmp' sed.data
 echo '==================================='
-sed -n 'N;s/special\nthis/this\nthis/gp' input
+
+echo 读入文件
+sed '3r sed.tmp' sed.data
 echo '==================================='
-sed -n '/special/{
-    p
+
+echo 修改行,用文件替换
+sed '2{
+    r sed.tmp
+    d
+    }' sed.data
+echo '==================================='
+
+echo 移动到下一行继续执行命令
+sed '/that/{n; d}' sed.data
+echo '==================================='
+
+echo 将两行合并为一行进行处理
+sed '=' sed.data | sed -n 'N; s/\n/ /1p' 
+echo '==================================='
+
+echo 多行打印，只打印第一行P
+sed '=' sed.data | sed -n 'N; /1/P' 
+    echo '==================================='
+
+echo 多行删除，只删除第一行D
+sed '=' sed.data | sed 'N; /1\n/D' 
+echo '==================================='
+
+
+#h 模式空间>保持空间
+#H 模式空间>>保持空间
+#g 保持空间>模式空间
+#G 保持空间>>模式空间
+#x 交换保持空间模式空间
+echo 保持空间
+sed -n '/this/{
     h
+    p
     n
     p
     g
     p
-    }' input
+    }' sed.data
 echo '==================================='
+
+echo 排除空间, 反转文本
 sed -n '{
     1!G
     h
     $p
-   } ' input
+    } ' sed.data
+
