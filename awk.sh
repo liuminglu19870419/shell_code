81f45e60d722995752f69cdf9d986800e23a7a80
minglu <minglu@minglu-virtual-machine.(none)>
2014-09-10 22:32:39 +0800

    add awk demo

--- awk.sh
+++ awk.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-gawk -f script.awk n=2 input
+#awk 使用
+
+#awk 参数
+#-F fs:行内字段分隔符
+#-f file:awk脚本文件名
+#-v var=value:设置一个变量
+#-mf N:最大字段数
+#-mr N:最大行数
+awk -F: '{print $1}' /etc/passwd
+echo ===============================
+
+#一般使用,调用脚本,设置变量
+awk -f awk.script end="this is end" awk.data
